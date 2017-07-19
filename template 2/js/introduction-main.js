e0c9a0c110b02b80a515c4f3a450a491316c8290
resilient-coders-11 <resilient-coders-11@resilient-coders-11s-MacBook-Air.local>
2017-07-19 12:16:13 -0400

    i fucked up

---
+++ template 2/js/introduction-main.js
@@ -0,0 +1,8 @@
+var total = 0, count = 1;
+while (count <= 10) {
+  total += count;
+  count += 1;
+}
+console.log(total);
+
+console.log(sum(range(1, 10)));
---
+++ template 2/js/program-structure-main.js
@@ -0,0 +1,26 @@
+1;
+!false;
+
+var caught = 5 * 5;
+
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+
+var one = 1, two = 2
+console.log(one + two);
+
+var ten = 10;
+console.log(ten * ten);
+
+var mood = "light";
+console.log(mood);
+mood = "dark";
+console.log(mood)
+
+var x = 30;
+console.log("the value of x is", x);
+
+console.log(Math.max(2, 4));
+
+console.log(Math.min(2, 4) +100);
